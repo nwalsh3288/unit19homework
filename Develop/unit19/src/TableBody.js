472ea77cee251846f4cd86bc17850e31488fc5df
Nicole Walsh <nmwalsh92@gmail.com>
2020-08-23 10:39:22 -0700

    final

--- Develop/unit19/src/TableBody.js
+++ Develop/unit19/src/TableBody.js
@@ -3,26 +3,69 @@ import data from "./data.json"
 
  
 class TableBody extends Component {
-    state = { 
-        sortStatus: "asc",
-        dataSort: [...data]
-     }
+    constructor(){
+        super()
+        this.state = { 
+            sortStatus: "asc",
+            dataSort: [...data],
+            input:""
+         }
+         this.inputChangeHandler = this.inputChangeHandler.bind(this)
+    }
+    
 
     // componentDidMount() {
     //     console.log(data)
     // }
 
     sorter = () => {
-        dataSort.sort((a,b)) => 
+        this.state.dataSort.sort((a, b) => {
+        if (a.name < b.name) {
+          return this.state.sortStatus === "asc" ? -1 : 1;
+        }
+        if (a.name > b.name) {
+            return this.state.sortStatus === "asc" ? 1 : -1;
+        }
+        return 0;
+      });
+      this.setState({sortStatus:this.state.sortStatus=== "asc" ? "desc": "asc"})
+      this.setState({dataSort: this.state.dataSort})
     }
 
+    inputChangeHandler(e){
+        const newValue = e.target.value
+        console.log(e.target.value)
+        console.log(this.state.input+"pre-input set state")
+        this.setState({
+            input: newValue
+        }
+        
+    ,() => this.finishFilter())}
+
+    finishFilter(){
+        console.log(this.state.input+"post-input set state")
+        let newData = this.state.dataSort.filter((d)=>{
+            console.log("new data function running")
+            return this.state.input === "" || d.name.includes(this.state.input)
+            
+          });
+          this.setState({
+            dataSort: newData
+        })
+        console.log(this.state.dataSort)
+        // try to reset the input
+        if(this.state.input === ""){
+            this.setState({dataSort:[...data]})
+        }
+    }
 
     render() { 
         return ( 
-            <>
+            <> {"Case Sensitive Filter"}
+            <input type="text" value={this.state.input} onChange={(e) => this.inputChangeHandler(e)}/>
         <tr>
         <th onClick={this.sorter}>
-        Name
+        Name (Click Me to Sort)
         </th>
         <th>
         Email
